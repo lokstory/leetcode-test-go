8f6b9c389a999e2d737b091111777bfa42cdbac2
Lucas Jiang <lokstory@gmail.com>
2019-05-13 15:52:29 +0800

    Create main.go

---
+++ 17. Letter Combinations of a Phone Number/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Given a string containing digits from 2-9 inclusive,
+// return all possible letter combinations that the number could represent.
+//
+// A mapping of digit to letters (just like on the telephone buttons) is given below.
+// Note that 1 does not map to any letters.
+
+// Input: "23"
+// Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+var charMap = map[rune][]rune{
+	'2': {'a', 'b', 'c'},
+	'3': {'d', 'e', 'f'},
+	'4': {'g', 'h', 'i'},
+	'5': {'j', 'k', 'l'},
+	'6': {'m', 'n', 'o'},
+	'7': {'p', 'q', 'r', 's'},
+	'8': {'t', 'u', 'v'},
+	'9': {'w', 'x', 'y', 'z'},
+}
+
+func letterCombinations(digits string) []string {
+	var result []string
+
+	if len(digits) == 0 {
+		return result
+	}
+
+	for _, key := range digits {
+		list, ok := charMap[key]
+		if !ok {
+			continue
+		}
+
+		if len(result) == 0 {
+			for _, c := range list {
+				result = append(result, string(c))
+			}
+		} else {
+			var newList []string
+			for _, text := range result {
+				for _, c := range list {
+					newList = append(newList, text + string(c))
+				}
+			}
+			result = newList
+		}
+	}
+
+	return result
+}
+
+func wonderfulSolution(digits string) []string {
+	var result []string
+
+	return result
+}
+
+
+func main() {
+	fmt.Println(letterCombinations("23"))
+}
